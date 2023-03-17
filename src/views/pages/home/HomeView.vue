39a8afd0a46558d43c40305a2f9a25b436ec701b
mAthieu33290 <55395305+mAthieu33290@users.noreply.github.com>
2023-03-17 14:41:16 +0100

    #13/homepage done

--- src/views/pages/home/HomeView.vue
+++ src/views/pages/home/HomeView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="">
-    <h1>Home</h1>
-    <img alt="Vue logo" src="../../../assets/logo.png">
-    <p>Welcome to Your Vue.js + TypeScript App</p>
-    <router-link to="/ChessRules">À propos</router-link>
+  <div class="container">
+    <h1 class="text-center">Bienvenue sur Chaturanga, le nouveau jeu d'échecs en ligne !</h1>
+    <img src="../../../assets/board.png" class="rounded mx-auto d-block my-5 w-25">
+    <div class="col col-md-10 text-center card-body mx-auto">
+      <h2>Qui sommes nous ?</h2>
+
+      <p class="mb-4">Chaturanga est un site Web pour jouer aux échecs version sorcier entre amis ou avec des inconnus en
+        ligne</p>
+      <h2>Mais comment jouer à ce jeu ?</h2>
+      <p class="mb-4">Connectez-vous ou inscrivez-vous sur le site pour accéder à la page du jeu accessible depuis la
+        barre
+        de navigation !
+        <br>
+        Vous pourrez choisir votre mode de jeu (avec un ami en ligne ou avec un inconnu au hasard) et sélectionner le
+        niveau de difficulté du jeu.
+      </p>
+      <button class="btn btn-lg btn-danger mb-3 mt-3 ">
+        Jouez maintenant
+      </button>
+
+      <p>Vous ne connaissez pas les règles du jeu ? <router-link to="/ChessRules">Cliquez ici</router-link></p>
+
+
+    </div>
   </div>
 </template>
 
@@ -14,3 +33,5 @@ export default defineComponent({
   name: 'HomeView',
 });
 </script>
+
+<style scoped></style>
