cdb132a259e27e4097f0e93473d529861ba29e44
Matleftod <97225801+Matleftod@users.noreply.github.com>
2023-03-23 11:50:51 +0100

    fix profile v1

--- src/views/pages/MyAccount/MyAccountView.vue
+++ src/views/pages/MyAccount/MyAccountView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="row my-5">
-    <div class="col-md-3">
+  <div class="profileContainer my-5">
+    <div class="col-md-3 profileMenu">
       <div class="list-group">
+        <h2>{{ user.email }}</h2>
         <button type="button" class="list-group-item list-group-item-action" @click="currentView = 'my-information'">My information</button>
         <button type="button" class="list-group-item list-group-item-action" @click="currentView = 'my-friends'">My friends</button>
         <button type="button" class="list-group-item list-group-item-action" @click="currentView = 'my-statistics'">My statistics</button>
       </div>
     </div>
-    <div class="col-md-9">
+    <div class="col-md-7 profileInfo">
       <component :is="currentView" :user="user" />
     </div>
   </div>
@@ -40,6 +41,12 @@ export default {
 </script>
 
 <style scoped>
+.profileContainer{
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh; /* Ajouté pour s'étendre sur toute la hauteur de la fenêtre */
+  align-items: center;
+}
 .list-group {
   width: 100%;
 }
@@ -51,5 +58,22 @@ export default {
 .card-img-top {
   max-height: 300px;
   object-fit: cover;
+}
+
+.profileMenu{
+  background-color: #E0E0E0;
+  margin: 0px 40px;
+  padding: 20px;
+}
+
+.list-group-item{
+  background: none;
+  border: none;
+}
+
+.profileInfo{
+  background-color: #E0E0E0;
+  padding: 15px;
+  margin: 20px 0px;
 }
 </style>
