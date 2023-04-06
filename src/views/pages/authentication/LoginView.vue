4fcd8a34dbfa86fdc0611bf614efc37d9bd69557
Zephyor <87764327+Zephyor@users.noreply.github.com>
2023-04-06 16:46:03 +0200

    Theme + modif store

--- src/views/pages/authentication/LoginView.vue
+++ src/views/pages/authentication/LoginView.vue
@@ -1,85 +1,85 @@
 <template>
-    <div class="container my-5">
-        <h1 class="text-center">Connexion</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card p-4">
-                    <Form @submit="handleLogin()">
-                        <div class="form-group">
-                            <label for="email">Adresse e-mail</label>
-                            <ErrorMessage
-                                name="email"/>
-                            <Field
-                                :rules="formValidation.checkEmail"
-                                v-model="userData.email"
-                                type="email"
-                                class="form-control"
-                                id="email"
-                                placeholder="Entrez votre adresse e-mail"
-                                name="email"/>
-                        </div>
-                        <div class="form-group mt-3">
-                            <label for="password">Mot de passe</label>
-                            <ErrorMessage
-                                name="password"/>
-                            <Field
-                                :rules="formValidation.checkPassword"
-                                v-model="userData.password"
-                                type="password"
-                                class="form-control"
-                                id="password"
-                                placeholder="Entrez votre mot de passe"
-                                name="password"/>
-                        </div>
-                        <button type="submit" class="btn btn-primary mt-3">Connexion</button>
-                    </Form>
-                </div>
+  <div class="container my-5">
+    <h1 class="text-center">Connexion</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card p-4">
+          <Form @submit="handleLogin()">
+            <div class="form-group">
+              <label for="email">Adresse e-mail</label>
+              <ErrorMessage name="email" />
+              <Field
+                :rules="formValidation.checkEmail"
+                v-model="userData.email"
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="Entrez votre adresse e-mail"
+                name="email"
+              />
             </div>
+            <div class="form-group mt-3">
+              <label for="password">Mot de passe</label>
+              <ErrorMessage name="password" />
+              <Field
+                :rules="formValidation.checkPassword"
+                v-model="userData.password"
+                type="password"
+                class="form-control"
+                id="password"
+                placeholder="Entrez votre mot de passe"
+                name="password"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">
+              Connexion
+            </button>
+          </Form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-
-import {defineComponent} from 'vue';
-import {Form, Field, ErrorMessage} from 'vee-validate';
-import {FormValidationService} from '@/services/FormValidation.service'
+import { defineComponent } from 'vue';
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { FormValidationService } from '@/services/FormValidation.service';
 import UserService from '@/services/User.service';
-import {UserLoginResponse} from '@/types/users';
+import { UserLoginResponse } from '@/types/users';
 
 export default defineComponent({
-    name: "LoginView",
+  name: 'LoginView',
 
-    components: {Form, Field, ErrorMessage},
+  components: { Form, Field, ErrorMessage },
 
-    data() {
-        return {
-            userData: {
-                email: "",
-                password: "",
-            },
-            formValidation: FormValidationService,
+  data() {
+    return {
+      userData: {
+        email: '',
+        password: '',
+      },
+      formValidation: FormValidationService,
+    };
+  },
 
-        }
+  methods: {
+    /**
+     * @description connecter un utilisateur
+     * @return void
+     */
+    handleLogin(): void {
+      UserService.login(this.userData)
+        .then((Response: UserLoginResponse) => {
+          this.$store.dispatch('login', Response);
+          this.$router.push({ name: 'DASHBOARD' });
+        })
+        .catch((error) => {
+          console.log(error.response.data.message);
+        });
     },
-
-    methods: {
-
-        /**
-         * @description connecter un utilisateur
-         * @return void
-         */
-        handleLogin(): void {
-            UserService.login(this.userData).then((Response: UserLoginResponse) => {
-                this.$store.dispatch("login", Response);
-                this.$router.push({name: "DASHBOARD"});
-            }).catch( error => {
-                console.log(error.response.data.message)
-            })
-        }
-    }
-})
+  },
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/pages/home/HomeView.vue
+++ src/views/pages/home/HomeView.vue
@@ -1,40 +1,104 @@
 <template>
   <div class="container">
-      <h1 class="text-center">Bienvenue sur Chaturanga, le nouveau jeu d'échecs en ligne !</h1>
+    <h1 class="text-center">
+      Bienvenue sur Chaturanga, le nouveau jeu d'échecs en ligne !
+    </h1>
 
-      <div class="col text-center py-5 mt-5">
-          <img src="../../../assets/board.png" class="rounded w-50 h-50" alt="">
-      </div>
+    <div class="col text-center py-5 mt-5">
+      <img src="../../../assets/board.png" class="rounded w-50 h-50" alt="" />
+    </div>
 
     <div class="col col-md-10 text-center card-body mx-auto">
       <h2>Qui sommes nous ?</h2>
 
-      <p class="mb-4">Chaturanga est un site Web pour jouer aux échecs version sorcier entre amis ou avec des inconnus en
-        ligne</p>
-      <h2>Mais comment jouer à ce jeu ?</h2>
-      <p class="mb-4">Connectez-vous ou inscrivez-vous sur le site pour accéder à la page du jeu accessible depuis la
-        barre
-        de navigation !
-        <br>
-        Vous pourrez choisir votre mode de jeu (avec un ami en ligne ou avec un inconnu au hasard) et sélectionner le
-        niveau de difficulté du jeu.
+      <p class="mb-4">
+        Chaturanga est un site Web pour jouer aux échecs version sorcier entre
+        amis ou avec des inconnus en ligne
       </p>
-      <button class="btn btn-lg btn-danger mb-3 mt-3 ">
+      <h2>Mais comment jouer à ce jeu ?</h2>
+      <p class="mb-4">
+        Connectez-vous ou inscrivez-vous sur le site pour accéder à la page du
+        jeu accessible depuis la barre de navigation !
+        <br />
+        Vous pourrez choisir votre mode de jeu (avec un ami en ligne ou avec un
+        inconnu au hasard) et sélectionner le niveau de difficulté du jeu.
+      </p>
+      <button
+        v-if="theme.sub_color === 'sub_color_hufflepuff'"
+        :class="theme.navbar.navbar_bg"
+        class="btn btn-lg mb-3 mt-3"
+      >
         Jouez maintenant
       </button>
-
-      <p>Vous ne connaissez pas les règles du jeu ? <router-link to="/ChessRules">Cliquez ici</router-link></p>
-
-
+      <button
+        v-else
+        :class="theme.navbar.navbar_bg"
+        class="btn btn-dark btn-lg mb-3 mt-3"
+      >
+        Jouez maintenant
+      </button>
+      <p>
+        Vous ne connaissez pas les règles du jeu ?
+        <router-link to="/ChessRules">Cliquez ici</router-link>
+      </p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import {
+  DefaultTheme,
+  DefaultLogo,
+  DefaultNavCodeColor,
+} from '@/helpers/defaultTheme';
+import { PATHS_NAME } from '@/constants';
 
 export default defineComponent({
   name: 'HomeView',
+  data() {
+    return {
+      theme: DefaultTheme,
+      logos: DefaultLogo,
+      navCodeColor: DefaultNavCodeColor,
+      PATHS_NAME,
+    };
+  },
+  methods: {
+    changeTheme(index: number) {
+      switch (index) {
+        case 0:
+          this.theme.sub_color = 'sub_color_gryffindor';
+          break;
+        case 1:
+          this.theme.sub_color = 'sub_color_slytherin';
+          break;
+        case 2:
+          this.theme.sub_color = 'sub_color_ravenclaw';
+          break;
+        case 3:
+          this.theme.sub_color = 'sub_color_hufflepuff';
+          break;
+
+        default:
+          this.theme.sub_color = 'sub_color_gryffindor';
+          break;
+      }
+
+      this.theme.logo = this.logos[index];
+      this.theme.navbar = this.navCodeColor[index];
+      this.$store.dispatch('theme', this.theme);
+    },
+
+    /**
+     * Déconnecter un utilisateur
+     */
+  },
+  mounted() {
+    if (this.$store.getters.theme) {
+      this.theme = this.$store.getters.theme;
+    }
+  },
 });
 </script>
 
